26d5dc09f1e78472a3f9a3c4d683af1e926f0056
lladlam <lladlam@outlook.com>
2026-08-13 09:40:14 +0800

    chore: bump development version to 0.3.1-Dev

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lladlam.melox.android"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.3.0-Dev"
+        versionCode = 4
+        versionName = "0.3.1-Dev"
     }
 
     buildFeatures {
